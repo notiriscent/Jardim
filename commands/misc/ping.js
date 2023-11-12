7427d786a7bc98b6d373dcdc1cffe59d6a0e88c5
FelixFromDiscord <sidorovf996@gmail.com>
2023-11-12 22:42:31 +0300

    create a mysql pool and a basic mysql query
    a ready mysql server required

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const pool = require('../../poolConnection');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,5 +7,28 @@ module.exports = {
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
         await interaction.reply(`Pong! Hi, I'm ${interaction.client.user.tag}!`);
+
+        pool.getConnection((err, connection) => {
+            if(err) {
+                connection.release();
+                throw err;
+            }
+
+            // Insert the user in the database if it's not saved already.
+            connection.query(`INSERT INTO users (id, username)
+            SELECT * FROM (SELECT ? AS id, ? AS username) AS temp
+            WHERE NOT EXISTS (
+                SELECT username FROM users WHERE id = ?
+            ) LIMIT 1;`, [interaction.user.id, interaction.user.username, interaction.user.id], async (err, results) => {
+                if(err) {
+                    connection.release();
+                    throw err;
+                }
+                
+                if(results.affectedRows > 0) {
+                    await interaction.followUp({ content: 'Added you to our database!', ephemeral: true });
+                }
+            });
+        });
     },
 };
---
+++ poolConnection.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+require('dotenv').config();
+
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    debug: false
+});
+
+module.exports = pool;
